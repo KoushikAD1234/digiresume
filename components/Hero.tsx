9c6a512426e956daf0bf1fd1ccc9f4b2b3232f1c
Koushik <koushikjio1234@gmail.com>
2024-07-29 16:07:21 +0530

    changes done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
 
             <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into seamless experiences" />
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg: text-2xl">Hi! I&apos;m Koushik, a Software Engineer based in India.</p>
-            <a href="http://">
+            <a href="#projects">
                 <MagicButton title="Show my work" icon={<FaLocationArrow />} position='right' />
             </a>
         </div>
